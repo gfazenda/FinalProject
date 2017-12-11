5d9d1d549f37465db21162005f04b3e6eef7242f
gfazenda <gabriel.l.fazenda@gmail.com>
2017-12-11 00:30:52 -0200

    firebase beginning + article

---
+++ Assets/Scripts/Utilities/PauseGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+    public List<GameObject> objsToDeactivate = new List<GameObject>();
+
+        bool gameIsPaused = false;
+
+        void Start()
+        {
+            this.gameObject.SetActive(!gameIsPaused);
+        }
+
+
+        public void Pause()
+        {
+            Time.timeScale = gameIsPaused ? 0 : 1;
+            this.gameObject.SetActive(gameIsPaused);
+            ChangeUIObjects();
+            //Disable scripts that still work while timescale is set to 0
+        }
+
+        private void ChangeUIObjects(){
+            for (int i = 0; i < objsToDeactivate.Count; i++)
+            {
+                objsToDeactivate[i].SetActive(gameIsPaused);
+            }
+        }
+}
+
