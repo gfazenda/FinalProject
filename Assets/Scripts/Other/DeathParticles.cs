d6f624d934e63a4bb413d36a7aeb7f50f77b9e8b
gfazenda <gabriel.l.fazenda@gmail.com>
2018-04-29 13:05:36 -0300

    new death prefabs

--- Assets/Scripts/Other/DeathParticles.cs
+++ Assets/Scripts/Other/DeathParticles.cs
@@ -6,6 +6,7 @@ public class DeathParticles : MonoBehaviour {
     public List<Color> colors;
     ParticleSystem _particleSystem;
     ParticleSystem.Particle[] particles;
+    ParticleSystem.MainModule mainModule;
     private int totalParticles;
     Color color;
 
@@ -13,35 +14,36 @@ public class DeathParticles : MonoBehaviour {
     // Use this for initialization
     void Start () {
         _particleSystem = this.GetComponent<ParticleSystem>();
-       
+        mainModule = _particleSystem.main;
+        mainModule.startColor = new ParticleSystem.MinMaxGradient(colors[0],colors[1]);
     }
 	
 	// Update is called once per frame
-	void Update () {
+	//void Update () {
 		
-	}
+	//}
 
-    private void LateUpdate()
-    {
-        if (dothing)
-        {
-            totalParticles = _particleSystem.GetParticles(particles);
-            for (int i = 0; i < totalParticles; i++)
-            {
-                color = colors[Random.Range(0, colors.Count - 1)];
-                particles[i].startColor = color;
-            }
+ //   private void LateUpdate()
+ //   {
+ //       if (dothing)
+ //       {
+ //           totalParticles = _particleSystem.GetParticles(particles);
+ //           for (int i = 0; i < totalParticles; i++)
+ //           {
+ //               color = colors[Random.Range(0, colors.Count - 1)];
+ //               particles[i].startColor = color;
+ //           }
 
-            _particleSystem.SetParticles(particles, totalParticles);
-            dothing = false;
-        }
+ //           _particleSystem.SetParticles(particles, totalParticles);
+ //           dothing = false;
+ //       }
       
-    }
+ //   }
 
 
-    private void OnEnable()
-    {
-        dothing = true;
-    }
+ //   private void OnEnable()
+ //   {
+ //       dothing = true;
+ //   }
 
 }
